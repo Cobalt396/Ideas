cc67b7ade855a07943fbc1d01712230cd4bc8e81
Daniel Neamati <danineamati@gmail.com>
2018-09-06 20:08:31 -0400

    Obtains Pokemon Information
    Relevant Images also downloaded.

--- Pokemon Team/getPokemon.py
+++ Pokemon Team/getPokemon.py
@@ -7,10 +7,13 @@
 import requests
 import pandas as pd
 import numpy as np
+from bs4 import BeautifulSoup as BS
+from tkinter import *
 
 print('Reading National Pokedex')
 
-def getPokemonFromBulba():
+def getHTMLpage():
+	'''Uses Request client to obtain the Bulbapedia page.'''
 	url = 'https://bulbapedia.bulbagarden.net/wiki/' + \
  		'List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number'
 
@@ -23,11 +26,30 @@ def getPokemonFromBulba():
 	}
 
 	r = requests.get(url, headers=header)
-	dfs = pd.read_html(r.text)
+
+	return r.text
+
+def getPokemonFromBulba():
+	'''Convert HTML page into a data frame for pandas.'''
+	dfs = pd.read_html(getHTMLpage())
 
 	return dfs
 
+def getPokemonImageBulba(pokemonName):
+	'''Gets specific pokemon image from webpage.'''
+	soup = BS(getHTMLpage(), features = "lxml")
 
+	# Ex: 'img alt="Umbreon"'
+	for imgtag in soup.find_all("img", {"alt": pokemonName}): 
+		filename = imgtag['src']
+		print(filename)
+		print(imgtag)
+
+	return imgtag
+
+def getListFromCSV(filename):
+	pokeList = pd.read_csv(filename,index_col = 'Number')
+	return pokeList
 
 def changeValToInt(dexFrame, col):
 	'''Ex: Changes #001 to 1'''
@@ -44,8 +66,7 @@ def changeValToInt(dexFrame, col):
 def formatRegionalDex(dexFrame, verbose = False):
 	'''Takes the HTML version and makes a useable form of the regional
 	pokedex.'''
-	# Rename the dual type to 'Type 2'
-	dexFrame.at[0, 5] = 'Type 2'
+	
 
 	# Change the format of the dex numbering
 	changeValToInt(dexFrame, 1) # National pokedex (Ndex)
@@ -58,8 +79,10 @@ def formatRegionalDex(dexFrame, verbose = False):
 		print('New Head is ', dexFrame.head(25))
 
 	# Set the column labels to the first row, namely:
-	# 'Kdex', 'Ndex', 'Pokemon', 'Type', 'Type 2'
-	dexFrame.columns = dexFrame.loc[0]
+	regionDex = dexFrame.at[0, 0]
+	col_names = [regionDex, 'Ndex', 'MS', 'Pokemon', 'Type', 'Type 2']
+	dexFrame.columns = col_names
+	# dexFrame.columns = dexFrame.loc[0]
 
 	# Now that the columns are relabelled, drop the duplicate row
 	dexFrame = dexFrame.drop(0)
@@ -76,8 +99,15 @@ def formatRegionalDex(dexFrame, verbose = False):
 
 	return dexFrame
 
-
-
+def printMergeInfo(filename, nationalDex):
+	'''Reads a csv and prints info from national pokedex.'''
+	infoFrame = getListFromCSV(filename)
+	if 'Pokemon' in infoFrame.columns.values and \
+				'Pokemon' in nationalDex.columns.values:
+		info = pd.merge(nationalDex.reset_index(), infoFrame, \
+			on = 'Pokemon', how = 'inner')
+		print(info)
+		print()
 
 def main():
 	'''Gets the national pokedex, formats each regional dex, and appends them
@@ -88,12 +118,22 @@ def main():
 	johto = formatRegionalDex(dfs[2])
 	hoenn = formatRegionalDex(dfs[3])
 	sinnoh = formatRegionalDex(dfs[4])
+	unova = formatRegionalDex(dfs[5])
+	print(dfs[6])
 
 	national = pd.concat([kanto, johto, hoenn, sinnoh], sort = 'False')
-	natCols = ['Kdex', 'Jdex', 'Hdex', 'Sdex', 'Pokémon', 'Type', 'Type 2']
+	natCols = ['Kdex', 'Jdex', 'Hdex', 'Sdex', 'Pokemon', 'Type', 'Type 2']
 	national = national[natCols]
 
-	print(national)
+	print(national.head(12))
+	print()
+
+	# printMergeInfo('RidePokemon.csv', national)
+	# printMergeInfo('ShoulderPokemon.csv', national)
+	printMergeInfo('danielTeam.csv', national)
+	printMergeInfo('josephTeam.csv', national)
+
+	getPokemonImageBulba('Umbreon')
 
 if __name__ == '__main__':
 	main()
